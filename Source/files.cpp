2d6142bb8087296e63e7e6102420b508f88299e4
johla342 <johla342@student.liu.se>
2009-12-01 21:27:56 +0100

    Some small alterations to files.h and files.cpp. Doesn't work and is commented out

--- Source/files.cpp
+++ Source/files.cpp
@@ -3,3 +3,22 @@
  */
 #include "common.h"
 
+/*
+ * Open level file at 'Level/{name}.lev and returns file handle
+ */
+/*ofstream open_level_file(string name) {
+	// Open file for reading only
+	ofstream file;
+	//file.open(path_from_level_name(name).c_str(),ios::in);
+	return file;
+}*/
+
+/*
+ * Get path to level with given name
+ */
+/*string path_from_level_name(string name) {
+	string path = "Level\\";
+	//path.append(name);
+	//path.append(".lev");
+	return path;
+}*/
