b74632d41f2bad9c8453cc29b6c825fb52ce1c78
johla342 <johla342@student.liu.se>
2009-12-04 14:02:24 +0100

    Added stubs for event handlers to allow working process on game.

--- Source/simulator_event_handler.cpp
+++ Source/simulator_event_handler.cpp
@@ -3,4 +3,43 @@
  */
 #include "common.h"
 
-//
+/*
+ * Simulator mouse movement handler
+ */
+void simulator_event_handler::e_mouse_move(int mouse_x, int mouse_y) {
+
+}
+
+/*
+ * Simulator mouse button handlers
+ */
+void simulator_event_handler::e_mouse_down(int mouse_x, int mouse_y, int button) {
+
+}
+void simulator_event_handler::e_mouse_up(int mouse_x, int mouse_y, int button) {
+
+}
+
+/*
+ * Simulator key button handlers
+ */
+void simulator_event_handler::e_key_down(int key) {
+
+}
+void simulator_event_handler::e_key_up(int key) {
+
+}
+
+/*
+ * Simulator new frame handler
+ */
+void simulator_event_handler::e_new_frame() {
+
+}
+
+/*
+ * Simulator step handler
+ */
+void simulator_event_handler::e_step(int delta_t) {
+
+}
